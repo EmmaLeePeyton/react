ebf74b48d036f4a9e9c95ef80c763ab495806896
EmmaLee Peyton <epeyton@email.sc.edu>
2024-10-23 22:38:28 -0400

    I think I did well

---
+++ react-01/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h1>Hello World!</h1>
+    <p>This was better than I thought! I am now worried for the rest of the assignments...</p>
+    </div>
+  );
+}
+
+export default App; // need to export to be able to import later
